1e4dc764c51594107f131a63d0e98c2c804eee54
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-04-13 22:13:00 -0400

    Start clearing controller.

---
+++ Nexus.Clearing.Server/Controllers/ClearingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nexus.Clearing.Server.Controllers;
+
+[ApiController]
+[Route("/clearing")]
+public class ClearingController : ControllerBase
+{
+    /// <summary>
+    /// Route for Roblox's webhook.
+    /// </summary>
+    [HttpPost]
+    [Route("roblox")]
+    public async Task<string> HandleRobloxWebhook()
+    {
+        // TODO: Actually handle webhook.
+        var content = await new StreamReader(this.Request.Body).ReadToEndAsync();
+        Logger.Debug("Starting request handling.");
+        foreach (var header in this.Request.Headers)
+        {
+            Logger.Debug($"Header {header.Key} = {header.Value}");
+        }
+        Logger.Debug($"Body: {content}");
+        return "Success";
+    }
+}
