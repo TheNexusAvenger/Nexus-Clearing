def4eddc668a24436867c749cf1e6a6472ec0cbd
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-04-14 09:39:53 -0400

    Add logging for exceptions.

--- Nexus.Clearing.Server/Program.cs
+++ Nexus.Clearing.Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using Nexus.Clearing.Server;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Nexus.Clearing.Server;
 using Nexus.Clearing.Server.Database;
 
 public class Program
@@ -20,11 +21,24 @@ public class Program
         // Build the server.
         Logger.Debug("Preparing server.");
         var builder = WebApplication.CreateBuilder(args);
-        builder.Host.ConfigureLogging(logging => logging.ClearProviders().AddProvider(Logger.NexusLogger));
+        builder.Logging.ClearProviders();
+        builder.Logging.AddProvider(Logger.NexusLogger);
         builder.Services.AddControllers();
 
         // Start the server.
         var app = builder.Build();
+        app.UseExceptionHandler(exceptionHandlerApp =>
+        {
+            exceptionHandlerApp.Run(context =>
+            {
+                var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                if (exceptionHandlerPathFeature != null)
+                {
+                    Logger.Error($"An exception occured processing {context.Request.Method} {context.Request.Path}\n{exceptionHandlerPathFeature.Error}");
+                }
+                return Task.CompletedTask;
+            });
+        });
         app.MapControllers();
         Logger.Info($"Starting server on port {Port}.");
         app.Run($"http://*:{Port}");
