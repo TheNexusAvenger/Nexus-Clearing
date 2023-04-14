08e1c2532e0527a85e8708d37013764581eec1fb
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-04-13 23:51:58 -0400

    Add signature verification.

---
+++ Nexus.Clearing.Server.Test/Util/VerificationTest.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Nexus.Clearing.Server.Util;
+using NUnit.Framework;
+
+namespace Nexus.Clearing.Server.Test.Util;
+
+public class VerificationTest
+{
+    /// <summary>
+    /// Valid signature from Roblox used for tests.
+    /// </summary>
+    public const string ValidSignatureHeader = "t=1681441534,v1=paQDFIOugCVzRKuJPoN2l9alCyBySHgIkUpbMIKlWJY=";
+    
+    /// <summary>
+    /// Valid secret used for tests.
+    /// </summary>
+    public const string ValidSecret = "test";
+    
+    /// <summary>
+    /// Valid body from Roblox used for tests.
+    /// </summary>
+    public const string ValidBody = "{\"NotificationId\":\"64c4e627-52dd-4b69-a918-e5a6aec43c2e\",\"EventType\":\"SampleNotification\",\"EventTime\":\"2023-04-14T03:05:34.4767037Z\",\"EventPayload\":{\"UserId\":25691148}}";
+    
+    /// <summary>
+    /// Tests VerifyRequest with a valid signature.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequest()
+    {
+        Assert.That(Verification.VerifyRequest(ValidSignatureHeader, ValidSecret, ValidBody), Is.EqualTo(true));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with an invalid signature due to the secret.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestBadSecret()
+    {
+        Assert.That(Verification.VerifyRequest(ValidSignatureHeader, "tent", ValidBody), Is.EqualTo(false));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with an invalid signature due to the timestamp.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestBadTimestamp()
+    {
+        Assert.That(Verification.VerifyRequest("t=1681441531,v1=paQDFIOugCVzRKuJPoN2l9alCyBySHgIkUpbMIKlWJY=", ValidSecret, ValidBody), Is.EqualTo(false));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with an invalid signature due to the body.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestBadBody()
+    {
+        Assert.That(Verification.VerifyRequest(ValidSignatureHeader, ValidSecret, "{\"NotificationId\":\"74c4e627-52dd-4b69-a918-e5a6aec43c2e\",\"EventType\":\"SampleNotification\",\"EventTime\":\"2023-04-14T03:05:34.4767037Z\",\"EventPayload\":{\"UserId\":25691148}}"), Is.EqualTo(false));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with an invalid signature due to the signature.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestBadSignature()
+    {
+        Assert.That(Verification.VerifyRequest("t=1681441534,v1=1aQDFIOugCVzRKuJPoN2l9alCyBySHgIkUpbMIKlWJY=", ValidSecret, ValidBody), Is.EqualTo(false));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with no signature.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestNoSignature()
+    {
+        Assert.That(Verification.VerifyRequest("t=1681441534", ValidSecret, ValidBody), Is.EqualTo(false));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with an invalid timestamp header.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestInvalidTimestampHeader()
+    {
+        Assert.That(Verification.VerifyRequest("t2=1681441534,v1=paQDFIOugCVzRKuJPoN2l9alCyBySHgIkUpbMIKlWJY=", ValidSecret, ValidBody), Is.EqualTo(false));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with a value in the header not set.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestUnsetValueHeader()
+    {
+        Assert.That(Verification.VerifyRequest("t2=1681441534,v1", ValidSecret, ValidBody), Is.EqualTo(false));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with a HttpRequest.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestHttpRequest()
+    {
+        var context = new DefaultHttpContext();
+        context.Request.Headers.Add("roblox-signature", ValidSignatureHeader);
+        Assert.That(Verification.VerifyRequest(context.Request, ValidSecret, ValidBody), Is.EqualTo(true));
+    }
+    
+    /// <summary>
+    /// Tests VerifyRequest with a HttpRequest but with no request.
+    /// </summary>
+    [Test]
+    public void TestVerifyRequestHttpRequestNoHeader()
+    {
+        var context = new DefaultHttpContext();
+        Assert.That(Verification.VerifyRequest(context.Request, ValidSecret, ValidBody), Is.EqualTo(false));
+    }
+}
---
+++ Nexus.Clearing.Server/Util/Verification.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Nexus.Clearing.Server.Util;
+
+public class Verification
+{
+    /// <summary>
+    /// Determines if a signature matches a request.
+    /// </summary>
+    /// <param name="signatureHeader">Header containing the signature.</param>
+    /// <param name="secret">Secret used for the HMAC SHA256 hash.</param>
+    /// <param name="body">Body of the request part of the signature.</param>
+    /// <returns>Whether the signature is valid or not.</returns>
+    public static bool VerifyRequest(string signatureHeader, string secret, string body)
+    {
+        // Get the timestamp and signature.
+        var headerParameters = new Dictionary<string, string>();
+        foreach (var headerPart in signatureHeader.Split(","))
+        {
+            if (!headerPart.Contains("=")) return false;
+            var splitHeaderParts = headerPart.Split("=", 2);
+            headerParameters[splitHeaderParts[0]] = splitHeaderParts[1];
+        }
+        if (!headerParameters.ContainsKey("t") || !headerParameters.ContainsKey("v1"))
+        {
+            return false;
+        }
+        var timestamp = headerParameters["t"];
+        var signature = headerParameters["v1"];
+        
+        // Compute the hash for the body.
+        using var sha256 = new HMACSHA256(Encoding.UTF8.GetBytes(secret));
+        var inputText = $"{timestamp}.{body}";
+        var newSignature = Convert.ToBase64String(sha256.ComputeHash(Encoding.UTF8.GetBytes(inputText)));
+        
+        // Return if the signatures match.
+        return signature == newSignature;
+    }
+    
+    /// <summary>
+    /// Determines if a signature matches a request.
+    /// </summary>
+    /// <param name="request">Request to verify against.</param>
+    /// <param name="secret">Secret used for the HMAC SHA256 hash.</param>
+    /// <param name="body">Body of the request part of the signature.</param>
+    /// <returns>Whether the signature is valid or not.</returns>
+    public static bool VerifyRequest(HttpRequest request, string secret, string body)
+    {
+        if (!request.Headers.TryGetValue("roblox-signature", out var headerValues)) return false;
+        if (headerValues.Count == 0) return false;
+        return VerifyRequest(headerValues[0]!, secret, body);
+    }
+}
